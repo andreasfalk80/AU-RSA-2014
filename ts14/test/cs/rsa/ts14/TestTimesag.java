0d381e708b9098cbc8dd65ec95ff5312eb1b143f
Andreas Falk <andreasfalk80@gmail.com>
2014-02-28 02:29:27 +0100

    Fixed error in spyWorkloadBuilder, where default locale is used, instead of specifying dansih, which would allow TestTimeSAg to run on all systems

--- ts14/test/cs/rsa/ts14/TestTimesag.java
+++ ts14/test/cs/rsa/ts14/TestTimesag.java
@@ -17,13 +17,10 @@ package cs.rsa.ts14;
 
 import org.junit.*; 
 
-import java.util.Locale;
-
 import cs.rsa.ts14.doubles.*;
 import cs.rsa.ts14.framework.*;
 import cs.rsa.ts14.standard.StandardTimesagLineProcessor;
 import static org.junit.Assert.*; 
-import cs.rsa.ts14.bravo.BravoLineTypeClassifierStrategy;
 
 /** JUnit learning tests. 
  * 
@@ -63,8 +60,7 @@ public class TestTimesag {
     // As builder, we use a partial implementation
     //
     builder = new SpyWorkloadBuilder();
-    //classifier = new FaultyLineTypeClassifierStrategy();
-    classifier = new BravoLineTypeClassifierStrategy();
+    classifier = new FaultyLineTypeClassifierStrategy();
     sequenceState = new LineSequenceStateStub();
     // Configure the standard TS14 line processor
     processor = 
@@ -127,16 +123,11 @@ public class TestTimesag {
   
   @Test
   public void shouldGenerateReportForWeek2() {
-    //create new Locale, to make sure the deciml point is '.' and not ','
-	Locale engLocale = new Locale("ENG");
-	
     processWeek2With8HoursWorkOnTaskN4C();
     processor.endProcess();
     String report = processor.getReport();
-	String testString = String.format(engLocale,"Week   %d :    %.1f hours   (  %d Wdays of   %.1f  d=%.1f)", 2, 8.0, 5, 8.0, -29.4);
     //             Week   1 :   14.5 hours   (  2 Wdays of   7.3  d=501.9)
-    //assertEquals( "Week   2 :    8,0 hours   (  5 Wdays of   8,0  d=-29,4)", report );  
-    assertEquals( testString, report );  
+    assertEquals( "Week   2 :    8,0 hours   (  5 Wdays of   8,0  d=-29,4)", report );  
   }
 
   private void processWeek2With8HoursWorkOnTaskN4C() {
